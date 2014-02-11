5b5de302e08762ead02bb9edc7bc094609cdcb0b
bryan newbold <bnewbold@leaflabs.com>
2014-02-11 14:07:13 -0500

    pypi packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='xmsgs-tools',
+      version='0.1',
+      description='Helper command line tools for parsing large Xilinx ISE FPGA build output logs',
+      url='http://github.com/leaflabs/xmsgs-tools/',
+      author='Bryan Newbold',
+      author_email='bnewbold@leaflabs.com',
+      license='MIT',
+      packages=['xmsgs'],
+      scripts=['xmsgsdiff', 'xmsgsprint'],
+      zip_safe=False)
