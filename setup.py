650fdb9f9f850b8b4136b33234261a9ed9bcbba7
bryan newbold <bnewbold@leaflabs.com>
2014-02-11 17:32:41 -0500

    bump to v0.2
    Signed-off-by: bryan newbold <bnewbold@leaflabs.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='xmsgs-tools',
-      version='0.1',
+      version='0.2.0',
       description='Helper command line tools for parsing large Xilinx ISE FPGA build output logs',
       url='http://github.com/leaflabs/xmsgs-tools/',
       author='Bryan Newbold',
